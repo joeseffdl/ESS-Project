b6745af17deaf732b74baf53e8fddd3e68fb6a8f
Joseph <joeseffdl@gmail.com>
2022-11-14 01:07:57 +0800

    Continued Login and SignUp UI
    Additional:
    Installed react icons and daisyui.
    Uninstalled material ui and react select dependencies.
    Experimented with colors using daisyUI.

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,8 +1,6 @@
 import Head from 'next/head'
 import Link from 'next/link'
 import Image from 'next/image'
-import Stack from '@mui/material/Stack'
-import Button from '@mui/material/Button'
 
 export default function Home() {
   return (
@@ -13,23 +11,21 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="w-screen p-20 bg-orange-100">
-        <section className="lg:my-20 md:my-28 my-40 md:flex md:flex-cols-2 font-bold">
+      <main className="w-screen bg-neutral text-primary-focus p-20">
+        <section className="lg:my-12 md:my-24 my-36 md:flex md:flex-cols-2 font-bold">
           <div className="md:w-1/2 flex flex-col justify-center"> 
             <div >
                 <p className="lg:text-4xl text-2xl ">Oregen <span className="md:text-2xl text-base">by GROUP 12</span></p>
                 <p className="lg:text-6xl text-4xl ">Research journal <br />template generator</p>
                 <Link href="/login">
-                  <Button
-                    className="
-                      lg:text-xl font-semibold 
-                      border-4 border-teal-500 text-teal-500 my-5 rounded-full 
-                      hover:border-black hover:text-black hover:border-4 hover:scale-105 transition ease-in-out hover:duration-300"
-                    size="medium"
-                    variant="outlined"
+                  <button
+                    className="btn btn-outline btn-accent
+                      lg:btn-lg
+                      border-4 rounded-full my-5
+                      hover:bg-none hover:scale-105 transition ease-in-out hover:duration-300"
                   >
                     Generate now
-                  </Button>
+                  </button>
                 </Link>
               </div>
             </div>  
@@ -43,7 +39,7 @@ export default function Home() {
               }
             ].map((landing) => {
               return (
-                <div className="grid place-content-center shadow-lg p-5 rounded-xl my-10 bg-white" key={landing}>
+                <div className="grid place-content-center drop-shadow-xl p-5 rounded-xl my-10 bg-neutral-focus" key={landing}>
                   <Image src={`/${landing.image}.jpg`} width={500} height={500} alt={`${landing.name} image`} />        
                 </div>
               )
@@ -66,11 +62,11 @@ export default function Home() {
               },
             ].map((features) => {
               return (
-                <div className="text-center shadow-lg p-10 rounded-xl lg:my-5 my-10 bg-white" key={features}>
+                <div className="text-center drop-shadow-xl p-10 rounded-xl lg:my-5 my-10 bg-neutral-focus" key={features}>
                     <div className="grid place-content-center">
                         <Image src={`/${features.image}.jpg`} width={100} height={100} alt={`${features.name} image`} />
                     </div>
-                    <p className="text-lg font-medium pt-8 pb-2">
+                    <p className="text-accent text-lg font-medium pt-8 pb-2">
                         { features.name }
                     </p>
                     <p className="py-2">
@@ -82,23 +78,24 @@ export default function Home() {
           </div>
           <div className="lg:w-1/2 lg:pl-20 flex flex-col justify-center">
             <div >
-              <p className="lg:text-5xl text-4xl font-bold ">Features</p>
+              <p className="text-secondary-focus lg:text-5xl text-4xl font-bold ">Features</p>
               <p className="lg:text-2xl text-xl my-5">Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga corrupti nesciunt eligendi sapiente laboriosam iusto, cum sit maxime, veniam dolorum cupiditate beatae quod. Repudiandae quod itaque saepe minus exercitationem a.</p>
             </div>
           </div>
         </section>
         <section className="lg:my-40 my-20">
           <div className="text-center">
-            <p className="text-3xl font-bold my-10">WHAT IS OREGEN <span className="text-base font-semibold">A BRIEF HISTORY</span></p>
+            <p className="text-3xl text-secondary-focus font-bold my-10">WHAT IS OREGEN <span className="text-base text-secondary font-semibold">A BRIEF HISTORY</span></p>
             <p className="text-2xl ">Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga corrupti nesciunt eligendi sapiente laboriosam iusto, cum sit maxime, veniam dolorum cupiditate beatae quod. Repudiandae quod itaque saepe minus exercitationem a.</p>
-            <Stack className="flex justify-center my-5" spacing={2} direction="row"> 
-              <Button className="rounded-full" variant="outlined">&lt;</Button>
-              <Button className="rounded-full" variant="outlined">&gt;</Button>
-            </Stack> 
+            
+            <div className="btn-group py-5">
+              <button className="btn btn-circle btn-outline ">&lt;</button>
+              <button className="btn btn-circle btn-outline ">&gt;</button>
+            </div>
           </div>
         </section>
         <section className="lg:mt-40 my-20 ">
-          <p className="text-4xl text-center font-bold py-2">The Team</p>
+          <p className="text-4xl text-center text-secondary-focus font-bold py-2">The Team</p>
           <div className="lg:grid lg:grid-cols-3 gap-10">
               {[
                   {
@@ -121,7 +118,7 @@ export default function Home() {
                   },
               ].map((team) => {
                   return (
-                      <div className="text-center shadow-lg p-10 rounded-xl my-10 bg-white" key={team}>
+                      <div className="text-center drop-shadow-xl p-10 rounded-xl my-10 bg-neutral-focus" key={team}>
                           <div className="grid place-content-center">
                               <Image src={`/${team.image}.jpg`} width={100} height={100} alt={`${team.name} image`} />
                           </div>
@@ -134,7 +131,7 @@ export default function Home() {
                           <ul>
                               {team.skills.map((skill) => {
                                   return (
-                                      <li className="text-teal-600 font-semibold py-1" key={skill}>
+                                      <li className="text-accent font-semibold py-1" key={skill}>
                                           {skill}
                                       </li>
                                   )
