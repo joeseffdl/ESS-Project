d79100d55520dc6bcb308bba2012ed0305aec2cb
Joseph <joeseffdl@gmail.com>
2022-12-29 20:09:05 +0800

    Updated Login Page

--- pages/login/index.jsx
+++ pages/login/index.jsx
@@ -58,41 +58,55 @@ function Login({ defaultUser = {} }) {
     }, [user])
 
     return (
-        <main className="w-screen h-screen text-primary-focus">
+        <main className="w-screen h-screen bg-slate-900">
             <nav className="py-8 font-bold">
-                    <div onClick={ () => setOpen(!open)} className="text-3xl absolute right-10 top-4 cursor-pointer md:hidden" >
+                    <div onClick={ () => setOpen(!open)} className="text-3xl text-white absolute right-10 top-4 cursor-pointer md:hidden" >
                         <span>
                             { open ? <AiOutlineClose /> : <AiOutlineMenu /> }
                         </span>
                     </div>
-                    <ul className={`px-10 md:flex md:items-center md:justify-between md:px-20 ${ open ? "absolute mt-10 md:mt-0" : "hidden"}`}>
+                    <ul className={`px-10 md:flex md:items-center md:justify-between md:px-24 ${ open ? "absolute mt-10 md:mt-0" : "hidden"}`}>
                         <li className="hover:translate-x-2 md:hover:-translate-x-2 ease-in-out duration-300">
                             <Link href="/">
-                                <p className="md:text-4xl text-3xl">Oregen</p>
+                                <p className="md:text-4xl text-3xl text-white font-extrabold">Oregen</p>
                             </Link>
                         </li> 
                     </ul>
             </nav>
-            <section className="w-full h-4/5 flex justify-center">
-                <div className="w-full h-full lg:flex hidden items-center justify-center">
+            <section className="w-full h-4/5 border-y-2 flex justify-center">
+                {/* <div className="w-full h-full lg:flex hidden items-center justify-center">
                     <Image src={loginImage} alt="Login Image" className="object-cover rounded-lg" />
-                </div>
+                </div> */}
                 <div className="container w-full">
                     <div className="h-full flex flex-col gap-3 justify-center items-center p-10">
-                        <h2 className="text-3xl font-bold text-center text-secondary pb-5">Login to your account</h2>
+                        <div>
+                            <h1 className="text-3xl font-bold text-center text-white mb-2
+                                sm:text-4xl md:text-5xl lg:text-6xl">Welcome to OREGEN
+                            </h1>
+                            <h2 className="text-lg text-center text-gray-200 mb-3
+                                md:text-xl lg:text-2xl">Log in to Oregen to continue
+                            </h2>
+                        </div>
                         
-                        <div className="flex justify-center items-center w-full max-w-lg" >
-                            <button onClick={GoogleLogin} className="btn btn-neutral w-full">
-                                <FcGoogle className="text-2xl mr-2" />
-                                Sign In with Google
-                            </button>
-                        </div> 
-                        <div className="flex justify-center items-center w-full max-w-lg" >
-                            <button onClick={GitHubLogin} className="btn btn-neutral w-full">
-                                <AiOutlineGithub className="text-2xl mr-3" />
-                                Sign In with GitHub
-                            </button>
-                        </div> 
+                        <div className="w-full flex flex-col gap-2 justify-center items-center
+                            md:flex-row">
+                            <div>
+                                <div className="flex justify-center items-center w-fit" >
+                                    <button onClick={GoogleLogin} className="w-full flex items-center justify-center font-semibold text-sm md:text-lg p-2 rounded-md bg-white text-gray-900 ">
+                                        <FcGoogle className="text-2xl md:text-3xl mr-2" />
+                                        Sign In with Google
+                                    </button>
+                                </div>
+                            </div> 
+                            <div>
+                                <div className="flex justify-center items-center w-fit" >
+                                    <button onClick={GitHubLogin} className="w-full flex items-center justify-center font-semibold text-sm md:text-lg p-2 rounded-md bg-slate-700 border-slate-700 text-white">
+                                        <AiOutlineGithub className="text-2xl md:text-3xl mr-2" />
+                                        Sign In with GitHub
+                                    </button>
+                                </div> 
+                            </div>
+                        </div>
                     </div>
                             
                 </div>
