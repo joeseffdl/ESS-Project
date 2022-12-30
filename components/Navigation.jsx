f7840cc87d78268b66658b2b2db4cd22d2429ee7
Joseph <joeseffdl@gmail.com>
2022-12-30 19:13:39 +0800

    Designed the account page of user

--- components/Navigation.jsx
+++ components/Navigation.jsx
@@ -28,13 +28,13 @@ function Navigation() {
     return (
         <main className={`text-gray-900 fixed w-full transition-colors ease-in-out duration-300 min-h-20 z-50 
                 ${scrollPosition > 0 || showNav ? 'bg-primary-focus text-white shadow-inner' : ''}`}>
-            <nav className="py-8">
+            <nav className="">
                 <div onClick={ () => setOpen(!open)} className="text-3xl absolute right-10 top-4 cursor-pointer md:hidden" >
                     <span>
                         { open ? <AiOutlineClose /> : <AiOutlineMenu /> }
                     </span>
                 </div>
-                <ul className={`px-10 ${scrollPosition > 900 ? 'md:w-full duration-300 ease-in-out' : 'md:w-1/2 duration-300 ease-in-out'} md:flex md:items-end md:justify-between xl:px-24 ${ open ? "block mt-10 md:mt-0" : "hidden"}`}>
+                <ul className={`px-10 py-5 ${scrollPosition > 900 ? 'md:w-full duration-300 ease-in-out' : 'md:w-1/2 duration-300 ease-in-out'} md:flex md:items-end md:justify-between xl:px-24 ${ open ? "block mt-10 md:mt-0" : "hidden"}`}>
                     <li className="hover:translate-x-2 md:hover:-translate-x-2 ease-in-out duration-300">
                         <Link href="/">
                             <p className="md:text-4xl text-3xl font-extrabold">Oregen</p>
