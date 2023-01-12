4c56655c13eac2e83cbdb8af50ce18b979a28f70
Joseph De Leon <joeseffdl@gmail.com>
2023-01-12 21:57:01 +0800

    Update AppNavigation.jsx

--- components/AppNavigation.jsx
+++ components/AppNavigation.jsx
@@ -21,6 +21,11 @@ function AppNavigation() {
     const handleWindowResize = () => {
         const width = window.innerWidth
         setWindowWidth(width)
+        if (windowWidth > 768) {
+            setOpen(false)
+        } else if (windowWidth == 768) {
+            setOpen(true)
+        }
     }
 
     useEffect(() => {
@@ -30,12 +35,14 @@ function AppNavigation() {
             window.removeEventListener("resize", handleWindowResize)
         }
     }, [])
+
+    console.log(open, windowWidth )
     
     return (
         <main className="text-white shadow-xl w-screen ">
-            <nav className={`flex items-center justify-between bg-gradient-to-b from-neutral-content to-primary ${open && windowWidth <= 768 ? "bg-gradient-to-b from-primary via-primary " : ""} py-5 px-10 xl:px-24 font-bold`}>
+            <nav className={`flex items-center justify-between ${open && windowWidth <= 768 ? "bg-primary" : "bg-gradient-to-b from-secondary to-primary"} py-5 px-10 xl:px-24 font-bold`}>
                 <div className="font-bold h-10 text-2xl cursor-pointer flex items-end">
-                    <div className="flex items-end"><Link href="/" className="md:text-4xl text-3xl font-extrabold hover:text-secondary-focus">Oregen</Link> <span className="hidden md:block ml-2 text-sm">Resume Template Builder</span></div>
+                    <div className="flex items-end"><Link href="/" className="text-4xl font-extrabold hover:text-secondary-focus">Oregen</Link> <span className="hidden sm:block ml-2 text-sm">Resume Template Builder</span></div>
                 </div>
                 <div onClick={ () => setOpen(!open)} className="text-3xl text-neutral-focus cursor-pointer md:hidden" >
                     <span>
@@ -44,7 +51,7 @@ function AppNavigation() {
                 </div>
                 <ul className={`h-10 pb-5 absolute left-0 w-full
                     md:flex md:items-end md:pb-0 md:z-auto md:pl-0 md:w-auto md:static
-                    ${open ? "top-16 z-50 h-1/4 border-b-2 border-accent bg-gradient-to-t from-neutral-content to-primary md:bg-none md:border-none" : "hidden"}`}>
+                    ${open ? "top-16 z-50 h-1/4 border-b-2 border-accent bg-gradient-to-t from-secondary to-primary md:bg-none md:border-none" : "hidden"}`}>
                     {
                         Links.map((link)=>(
                             <li key={link.name} className='text-2xl mt-5 pt-3 px-10
